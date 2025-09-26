c449f5be173e4b8efbf358b0f6addabd985805e5
learnuidev <learnuidev@gmail.com>
2025-09-25 21:00:14 -0400

    fix bug

--- constants/table-names.js
+++ constants/table-names.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 /* eslint-disable no-undef */
 const tableNames = {
   usersTable: process.env.USERS_TABLE,
-  userPreferenceTable: USER_PREFERENCE_TABLE,
+  userPreferenceTable: process.env.USER_PREFERENCE_TABLE,
   analyticsTable: process.env.ANALYTICS_TABLE,
   featureFlagsTable: process.env.FEATURE_FLAGS_TABLE,
 };
