b62f253424caa8b2e9b3e940a2c75634529e993b
learnuidev <learnuidev@gmail.com>
2025-09-27 17:21:44 -0400

    wip

--- src/functions/user-credentials/list-user-credentials.api.ts
+++ src/functions/user-credentials/list-user-credentials.api.ts
@@ -2,11 +2,13 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { apiConfig } from "../../constants/api-config.js";
 import { tableNames } from "../../constants/table-names.js";
+import { cryptoV2 } from "../../lib/crypto-v2.js";
+import { credentialsPrefix } from "./user-credentials.constants.js";
 
 const ddbClient = new DynamoDBClient({ region: apiConfig.region });
 const dynamodb = DynamoDBDocumentClient.from(ddbClient);
 
-export const listUserCredentialsApi = async (userId) => {
+export const listUserCredentialsApi = async (userId: string) => {
   const params = {
     TableName: tableNames.userCredentialsTable,
     IndexName: "byUserId",
@@ -16,10 +18,23 @@ export const listUserCredentialsApi = async (userId) => {
     },
   };
 
+  const crypto = cryptoV2({ keyArn: apiConfig.kmsArn || "" });
+
   const resp = await dynamodb.send(new QueryCommand(params));
 
-  return resp.Items.map((item) => ({
-    ...item,
-    apiSecret: `***********`,
-  }));
+  const items = resp?.Items;
+
+  if (!items) {
+    return [];
+  }
+
+  return await Promise.all(
+    items.map(async (item) => {
+      const { plaintext } = await crypto.decrypt(item?.apiSecret);
+      return {
+        ...item,
+        apiSecret: `${credentialsPrefix}${item?.apiKey}${plaintext}`,
+      };
+    })
+  );
 };
