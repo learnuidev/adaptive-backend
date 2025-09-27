f009a620be080838a74666d7b588f7ce44119990
learnuidev <learnuidev@gmail.com>
2025-09-27 12:37:03 -0400

    wip

--- src/functions/auth/validate-api-key.ts
+++ src/functions/auth/validate-api-key.ts
@@ -11,12 +11,15 @@ export async function validateApiKey(apiKeyWithMando: string) {
   // Implement your API key validation logic here
   // This could involve checking against a database or external service
 
-  const crypto = cryptoV2({ keyArn: apiConfig.kmsArn });
+  const crypto = cryptoV2({ keyArn: apiConfig.kmsArn || "" });
 
   const apiKey = apiKeyWithMando.split(credentialsPrefix)[1];
 
   const userCredentialId = apiKey.slice(0, API_KEY_LENGTH);
-  const apiSecretParams = apiKey.slice(API_KEY_LENGTH, API_SECRET_LENGTH);
+  const apiSecretParams = apiKey.slice(
+    API_KEY_LENGTH,
+    API_KEY_LENGTH + API_SECRET_LENGTH
+  );
 
   const userCredential = await getUserCredentialById(userCredentialId);
 
