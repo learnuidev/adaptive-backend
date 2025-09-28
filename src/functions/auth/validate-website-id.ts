7dc53761fbe6dd4f97e055ed65ba44b30fe94340
learnuidev <learnuidev@gmail.com>
2025-09-27 21:12:49 -0400

    code clean up

--- src/functions/auth/validate-website-id.ts
+++ src/functions/auth/validate-website-id.ts
@@ -1,5 +1,4 @@
 import { getUserCredentialById } from "../user-credentials/get-user-credential-by-id.api.js";
-import { generatePolicy } from "./generate-policy.js";
 
 export const validateWebSiteId = async (event: any) => {
   const websiteId = JSON.parse(event.body)?.websiteId;
@@ -13,23 +12,10 @@ export const validateWebSiteId = async (event: any) => {
   }
 
   const userCredential = await getUserCredentialById(websiteId);
-  // Extract userRequestDomain from event headers, e.g. Host header
 
   if (!userCredential) {
     throw new Error(errorMessage);
   }
-
-  if (!userRequestDomain) {
-    throw new Error(errorMessage);
-  }
-
-  // if (userCredential.domains) {
-  //   if (userCredential?.domains?.includes(userRequestDomain)) {
-  //     return generatePolicy("user", "Allow", event.methodArn, userCredential);
-  //   } else {
-  //     throw new Error(errorMessage);
-  //   }
-  // }
 
   if (!userRequestDomain?.includes(userCredential.domain)) {
     throw new Error(errorMessage);
